437524642aac1d7d631b759cc2530ed6b7c03bc0
Austen Edge <austen.edge@xtra.co.nz>
2023-11-30 09:43:48 +1300

    initial commit

---
+++ index.py
@@ -0,0 +1,56 @@
+import curses
+import time
+
+def main(stdscr):
+    # Initialize the screen
+    curses.initscr()
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+
+    # Set up the typing test
+    expected_text = "speed"
+    typed_text = ""
+    start_time = time.time()
+
+    # Set up the layout
+    stdscr.addstr(0, 5, f"Type the word: {expected_text}", curses.color_pair(1))
+    stdscr.addstr(2, 5, "Your input: ", curses.color_pair(2))
+    stdscr.addstr(4, 5, "Typing Speed: ", curses.color_pair(2))
+    stdscr.addstr(6, 5, "Words per Minute: ", curses.color_pair(2))
+    stdscr.addstr(8, 5, "Time Elapsed: ", curses.color_pair(2))
+
+    # Main loop to get user input
+    while typed_text != expected_text:
+        try:
+            key = stdscr.getkey()
+            if key.isalpha() or key.isspace():
+                typed_text += key
+
+                # Display typed text with color coding
+                for index, letter in enumerate(typed_text):
+                    correct_char = expected_text[index]
+                    text_color = curses.color_pair(1) if letter == correct_char else curses.color_pair(2)
+                    stdscr.addstr(2, 18 + index, letter, text_color)
+
+                stdscr.refresh()
+            elif key == '\n':
+                break
+        except curses.error:
+            pass
+
+    # Calculate typing speed and display results
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    words_per_minute = len(typed_text.split()) / elapsed_time * 60
+    characters_per_minute = len(typed_text) / elapsed_time * 60
+
+    stdscr.addstr(4, 21, f"{characters_per_minute:.2f} CPM", curses.color_pair(2))
+    stdscr.addstr(6, 23, f"{words_per_minute:.2f} WPM", curses.color_pair(2))
+    stdscr.addstr(8, 18, f"{elapsed_time:.2f} seconds", curses.color_pair(2))
+    stdscr.refresh()
+
+    # Wait for a key press before exiting
+    stdscr.getch()
+
+curses.wrapper(main)
